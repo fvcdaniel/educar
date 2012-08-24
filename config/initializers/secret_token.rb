2924ad47f87b7590c7c49a928f8b4d5ae596ec00
Your Name Here <fvcdaniel@gmail.com>
2012-08-24 11:42:13 -0300

    ïnitial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Educar::Application.config.secret_token = 'fc6fe9da189dbf2d2cd7b8b20a34333693c74ca1c01282e07ebb82fcf2e2bb86a747aabc600087af7bb082c4cc7cf232a67091abe30604b47edb82f1af1e404d'
