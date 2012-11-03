27d0b31030076dfe1860f89757d62b4eca685fad
Your Name Here <fvcdaniel@gmail.com>
2012-11-03 16:24:22 -0300

    teste

--- db/migrate/20121103190448_adiciona_role_admin.rb
+++ db/migrate/20121103190448_adiciona_role_admin.rb
@@ -1,9 +1,9 @@
-class AdicionaAdminUser < ActiveRecord::Migration
+class AdicionaRoleAdmin < ActiveRecord::Migration
   def up
-  	Role.create!(:nome => :admin)
+  	Role.create!(:nome => :admin) if Role.find_by_nome(:admin).blank?
   end
 
   def down
-  	Role.destroy(:nome => :admin)
+  	Role.destroy(:nome => :admin) unless Role.find_by_nome(:admin).blank?
   end
 end
