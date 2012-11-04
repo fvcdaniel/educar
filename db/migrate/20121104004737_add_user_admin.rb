8d1a68317fca8fa895be21e3c6e67c7e56c9f37b
Daniel <fvcdaniel@gmail.com>
2012-11-03 21:49:15 -0300

    sync

---
+++ db/migrate/20121104004737_add_user_admin.rb
@@ -0,0 +1,21 @@
+class AddUserAdmin < ActiveRecord::Migration
+  def up
+  	user = User.find_by_email('fvcdaniel@gmail.com')
+  	if user.blank?
+  		user = User.create!(:email => 'fvcdaniel@gmail.com', :password => 'fvc0902', :password_confirmation => 'fvc0902')
+  	end
+  	role = Role.find_by_name(:admin)
+  	unless user.blank? and role.blank?
+  		unless user.roles.include? role
+  			user.roles << role
+  			user.save
+  		end
+  	end
+  end
+
+  def down
+  	user = User.find_by_email('fvcdaniel@gmail.com')
+  	user.roles.delete Role.find_by_name(:admin)
+  	user.save
+  end
+end
