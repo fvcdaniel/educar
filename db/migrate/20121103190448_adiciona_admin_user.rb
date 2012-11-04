a8cd19d54d60a45ad544ccbf062daf9a82d53183
Daniel <fvcdaniel@gmail.com>
2012-11-03 21:22:44 -0300

    teste

--- db/migrate/20121103190448_adiciona_admin_user.rb
+++ db/migrate/20121103190448_adiciona_admin_user.rb
@@ -1,6 +1,9 @@
 class AdicionaAdminUser < ActiveRecord::Migration
   def up
   	user = User.find_by_email('fvcdaniel@gmail.com')
+  	if user.blank?
+  		user = User.create!(:email => 'fvcdaniel@gmail.com', :password => 'fvc0902', :password_confirmation => 'fvc0902')
+  	end
   	role = Role.find_by_name(:admin)
   	unless user.blank? and role.blank?
   		unless user.roles.include? role
