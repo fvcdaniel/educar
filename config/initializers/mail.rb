415cddce06407476fc8c4e8e663b857ed62b6e7d
Daniel <fvcdaniel@gmail.com>
2012-11-03 22:28:20 -0300

    exception notifier

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,5 @@
     ActionMailer::Base.smtp_settings = {  
       :address              => "127.0.0.1",  
       :port                 => 25,  
-      :domain               => "meuimovelpb.com",  
-      :user_name            => "daniel",  
-      #:password             => "secret",  
-      :authentication       => nil
-      #:enable_starttls_auto => true  
+      :domain               => "meuimovelpb.com"  
     }
