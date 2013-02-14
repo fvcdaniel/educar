9b032d1b4c48aa9ff6daf04247bc0f9eca8fb9db
Daniel <fvcdaniel@gmail.com>
2013-02-14 20:28:08 -0300

    teste

--- Gemfile
+++ Gemfile
@@ -19,7 +19,6 @@ gem "rmagick", "2.12.2"
 gem "carrierwave"
 gem 'ancestry'
 gem 'newrelic_rpm'
-gem 'omniauth'
 gem 'omniauth-facebook'
 
 group :development do
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,6 +14,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def passthru
+    a = 1 + '2'
     render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
     # Or alternatively,
     # raise ActionController::RoutingError.new('Not Found')
