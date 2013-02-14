ea6a20f4fec7bde319081c31ae7b9cc611016d6b
Daniel <fvcdaniel@gmail.com>
2013-02-14 19:05:02 -0300

    facebook impl

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,9 @@ Educar::Application.routes.draw do
 
   resources :materias
 
+  devise_scope :user do
+    get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
+  end
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   devise_for :users
